f8d8a8629a5b881a7dca6da698d37a954d6cc1c2
mm12346 <xoctell338@gmail.com>
2025-07-16 10:55:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Service Worker for FixFlow App (Debugging Version)
-console.log('Service Worker Loaded - v7.0 (Debug)');
+console.log('Service Worker Loaded - v8.0 (Debug)');
 
 // --- CACHE VERSION ---
 // Incrementing the version number is crucial for triggering the 'activate' event.
-const CACHE_NAME = 'webput-cache-v7.0'; 
+const CACHE_NAME = 'webput-cache-v8.0'; 
 const REPO_NAME = '/webput'; // << Your repository name on GitHub
 
 // A list of files to cache for the application shell.
